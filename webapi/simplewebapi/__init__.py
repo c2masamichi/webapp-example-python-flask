656aab8505fee2309736c395ec7ea1d103e37103
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-08-29 22:56:59 +0900

    get method

--- webapi/simplewebapi/__init__.py
+++ webapi/simplewebapi/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
 
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
+
+    @app.route('/product/<int:product_id>')
+    def get_product(product_id):
+        return 'get {0}'.format(product_id)
 
     return app
