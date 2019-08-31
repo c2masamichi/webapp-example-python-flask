9a88c1485a45b00ed274c56b242010cd68f16c86
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-08-31 18:49:50 +0900

    get by json

--- webapi/simplewebapi/__init__.py
+++ webapi/simplewebapi/__init__.py
@@ -1,7 +1,13 @@
+import os
+
 from flask import Flask
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        JSON_PATH=os.path.join(app.instance_path, 'data.json'),
+    )
 
     @app.route('/test')
     def hello_world():
--- webapi/simplewebapi/api.py
+++ webapi/simplewebapi/api.py
@@ -1,11 +1,26 @@
+import json
+
 from flask import Blueprint
+from flask import current_app
+from flask import jsonify
+from werkzeug.exceptions import abort
 
 bp = Blueprint('api', __name__)
+
 
 @bp.route('/')
 def index():
     return 'top'
 
+def load_data(product_id):
+    with open(current_app.config['JSON_PATH'], encoding='utf-8') as f:
+        data = json.load(f)
+        str_id = str(product_id)
+        if str_id not in data:
+            abort(404)
+        return data[str_id]
+
+
 @bp.route('/product/<int:product_id>')
 def get_product(product_id):
-    return 'get {0}'.format(product_id)
+    return jsonify(load_data(product_id))
