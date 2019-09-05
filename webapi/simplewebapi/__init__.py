5c3de166576cbe04cd02e52469493b755e7475b9
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-09-05 21:22:34 +0900

    make test

--- webapi/simplewebapi/__init__.py
+++ webapi/simplewebapi/__init__.py
@@ -3,11 +3,14 @@ import os
 from flask import Flask
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         DATABASE=os.path.join(app.instance_path, 'webapi.sqlite'),
     )
+
+    if test_config is not None:
+        app.config.update(test_config)
 
     @app.route('/healthcheck')
     def healthcheck():
