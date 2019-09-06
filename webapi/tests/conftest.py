ab839a6a7ed032d2da5de9d1589e1d183fcb0ef6
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-09-06 23:14:12 +0900

    test healthcheck

---
+++ webapi/tests/conftest.py
@@ -0,0 +1,28 @@
+import os
+import tempfile
+
+import pytest
+
+from simplewebapi import create_app
+from simplewebapi.db import get_db
+from simplewebapi.db import init_db
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+    app = create_app({'TESTING': True, 'DATABASE': db_path})
+
+    with app.app_context():
+        file_path = os.path.join(os.path.dirname(__file__), 'data.sql')
+        init_db(file_path)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
--- webapi/tests/test_factory.py
+++ webapi/tests/test_factory.py
@@ -7,3 +7,8 @@ def test_config():
     """
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
+
+
+def test_healthcheck(client):
+    response = client.get('/healthcheck')
+    assert response.data == b'app running'
