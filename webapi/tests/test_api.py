faf039dce2066f32482c8e77cc8bd29764badbe0
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-09-08 23:34:19 +0900

    test get_products

--- webapi/tests/test_api.py
+++ webapi/tests/test_api.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from simplewebapi.db import get_db
@@ -6,3 +8,11 @@ from simplewebapi.db import get_db
 def test_index(client):
     response = client.get('/')
     assert b'top' in response.data
+
+
+def test_get_products(client):
+    response = client.get('/products')
+    data = json.loads(response.data)
+    assert 'result' in data
+    result = data['result']
+    assert len(result) == 2
