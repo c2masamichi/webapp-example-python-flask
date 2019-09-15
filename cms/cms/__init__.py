e57ded7ddb9f47e6cc2b0c163afb280bf16524f0
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-09-15 18:51:58 +0900

    fix db

--- cms/cms/__init__.py
+++ cms/cms/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        DATABASE=os.path.join(app.instance_path, 'webapi.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'cms.sqlite'),
     )
 
     if test_config is not None:
