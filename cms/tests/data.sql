5c896891f08bc5dc875c86f09a0778e5d5b13c4c
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-09-16 15:02:24 +0900

    fix test data

--- cms/tests/data.sql
+++ cms/tests/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO post (title, body, created)
 VALUES
-  ('Test Title', 'This body is test.', '2019-01-01 00:00:00');
+  ('Test Title 1', 'This body is test.', '2019-01-01 00:00:00'),
+  ('Test Title 2', 'Test Test Test Test', '2019-01-01 12:30:45'),
+  ('Test Title 3', 'Test Test Test Test', '2019-01-02 08:20:01');
