157c8640adefc60fa8393a6f8c9865bb4057637b
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-09-28 23:30:40 +0900

    fix test data

--- cms/tests/data.sql
+++ cms/tests/data.sql
@@ -1,3 +1,7 @@
+INSERT INTO user (username, password)
+VALUES
+  ('testuser', 'pbkdf2:sha256:150000$uruHRLDK$e7291b6efdd7439668e47fbd509e9fadad0cd7511073fb47d9846161c9605e20');
+
 INSERT INTO post (title, body, created)
 VALUES
   ('Test Title 1', 'This body is test.', '2019-01-01 00:00:00'),
