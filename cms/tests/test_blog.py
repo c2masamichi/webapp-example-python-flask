a808b175b110c491d3fd02fcb193c6f02ce890a1
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-10-02 23:45:57 +0900

    test blog

---
+++ cms/tests/test_blog.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Test Title 1' in response.data
+    assert b'2019-01-01' in response.data
+    assert b'This body is test.' in response.data
+
+
+def test_get_detail(client):
+    response = client.get('/blog/1')
+    assert response.status_code == 200
+    assert b'Test Title 1' in response.data
+    assert b'2019-01-01' in response.data
+    assert b'This body is test.' in response.data
+
+
+def test_get_detail_error(client):
+    response = client.get('/blog/5')
+    assert response.status_code == 404
