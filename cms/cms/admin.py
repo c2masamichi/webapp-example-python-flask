bea0f25c0bcccc4c01beeb98ac0ce64fc8ac983f
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-10-13 18:33:56 +0900

    fix url

--- cms/cms/admin.py
+++ cms/cms/admin.py
@@ -9,75 +9,9 @@ from werkzeug.exceptions import abort
 from werkzeug.security import generate_password_hash
 
 from cms.auth import login_required
-from cms.blog import get_post
 from cms.db import get_db
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
-
-
-@bp.route('/create', methods=['GET', 'POST'])
-@login_required
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body) VALUES (:title, :body)',
-                {'title': title, 'body': body},
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('admin/create.html')
-
-
-@bp.route('/update/<int:post_id>', methods=['GET', 'POST'])
-@login_required
-def update(post_id):
-    post = get_post(post_id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = :title, body = :body WHERE id = :id',
-                {'title': title, 'body': body, 'id': post_id},
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('admin/update.html', post=post)
-
-
-@bp.route('/delete/<int:post_id>', methods=['POST'])
-@login_required
-def delete(post_id):
-    get_post(post_id)
-    db = get_db()
-    db.execute(
-        'DELETE FROM post WHERE id = :id',
-        {'id': post_id},
-    )
-    db.commit()
-    return redirect(url_for('blog.index'))
 
 
 @bp.route('/register', methods=['GET', 'POST'])
--- cms/tests/test_admin.py
+++ cms/tests/test_admin.py
@@ -6,75 +6,12 @@ from cms.db import get_db
 @pytest.mark.parametrize(
     'path',
     (
-        '/admin/create',
-        '/admin/update/1',
-        '/admin/delete/1',
         '/admin/register',
     )
 )
 def test_login_required(client, path):
     response = client.post(path)
     assert response.headers['Location'] == 'http://localhost/auth/login'
-
-
-@pytest.mark.parametrize(
-    'path',
-    ('/admin/update/5', '/admin/delete/5')
-)
-def test_exists_required(client, auth, path):
-    auth.login()
-    assert client.post(path).status_code == 404
-
-
-def test_create(client, auth, app):
-    auth.login()
-    assert client.get('/admin/create').status_code == 200
-    client.post(
-        '/admin/create',
-        data={'title': 'created', 'body': 'created on test'}
-    )
-
-    with app.app_context():
-        db = get_db()
-        post = db.execute('SELECT * FROM post WHERE id = 4').fetchone()
-        assert post['title'] == 'created'
-        assert post['body'] == 'created on test'
-
-
-def test_update(client, auth, app):
-    auth.login()
-    assert client.get('/admin/update/1').status_code == 200
-    client.post(
-        '/admin/update/1',
-        data={'title': 'updated', 'body': 'updated on test'}
-    )
-
-    with app.app_context():
-        db = get_db()
-        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
-        assert post['title'] == 'updated'
-        assert post['body'] == 'updated on test'
-
-
-@pytest.mark.parametrize(
-    'path',
-    ('/admin/create', '/admin/update/1')
-)
-def test_create_update_validate(client, auth, path):
-    auth.login()
-    response = client.post(path, data={'title': '', 'body': ''})
-    assert b'Title is required.' in response.data
-
-
-def test_delete(client, auth, app):
-    auth.login()
-    response = client.post('/admin/delete/1')
-    assert response.headers['Location'] == 'http://localhost/'
-
-    with app.app_context():
-        db = get_db()
-        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
-        assert post is None
 
 
 def test_register(client, auth, app):
