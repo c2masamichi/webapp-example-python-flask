3e4e85151fe991b168010285eb90b838597f3a18
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-10-31 23:10:10 +0900

    load config

--- webapi/webapi/__init__.py
+++ webapi/webapi/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from flask import Flask
 
@@ -9,7 +10,13 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'webapi.sqlite'),
     )
 
-    if test_config is not None:
+    if test_config is None:
+        try:
+            app.config.from_pyfile('config.py')
+        except FileNotFoundError:
+            print('[Error] instance/config.py must exist.', file=sys.stderr)
+            sys.exit(1)
+    else:
         app.config.update(test_config)
 
     @app.route('/healthcheck')
