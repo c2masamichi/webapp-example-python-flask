5467b82873d3f722511bc0946d7ae050410b715e
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-11-06 23:13:46 +0900

    load config in testing

--- webapi/tests/conftest.py
+++ webapi/tests/conftest.py
@@ -8,16 +8,7 @@ from webapi.db import init_db
 
 @pytest.fixture
 def app():
-    app = create_app(
-        {
-            'TESTING': True,
-            'DB_HOST': 'db',
-            'DB_PORT': 3306,
-            'DB_USER': 'dev_user',
-            'DB_PASSWORD': 'dev_pass',
-            'DATABASE': 'dev_db',
-        }
-    )
+    app = create_app()
 
     with app.app_context():
         init_db(withdata=True)
--- webapi/webapi/__init__.py
+++ webapi/webapi/__init__.py
@@ -4,17 +4,14 @@ import sys
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        try:
-            app.config.from_pyfile('config.py')
-        except FileNotFoundError:
-            print('[Error] instance/config.py must exist.', file=sys.stderr)
-            sys.exit(1)
-    else:
-        app.config.update(test_config)
+    try:
+        app.config.from_pyfile('config.py')
+    except FileNotFoundError:
+        print('[Error] instance/config.py must exist.', file=sys.stderr)
+        sys.exit(1)
 
     @app.route('/healthcheck')
     def healthcheck():
