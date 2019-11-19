84e3e1665dd1ea7ef6678a609c92fb1c343df0e2
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-11-20 00:03:59 +0900

    rename vars

--- cms/cms/blog.py
+++ cms/cms/blog.py
@@ -15,23 +15,23 @@ bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
-    posts = Entry().fetch_all()
-    return render_template('blog/index.html', posts=posts)
+    entries = Entry().fetch_all()
+    return render_template('blog/index.html', entries=entries)
 
 
-@bp.route('/entry/<int:post_id>')
-def get_entry(post_id):
-    post = Entry().fetch(post_id)
-    if post is None:
+@bp.route('/entry/<int:entry_id>')
+def get_entry(entry_id):
+    entry = Entry().fetch(entry_id)
+    if entry is None:
         abort(404)
-    return render_template('blog/detail.html', post=post)
+    return render_template('blog/detail.html', entry=entry)
 
 
 @bp.route('/edit/')
 @login_required
 def list_for_editors():
-    posts = Entry().fetch_all()
-    return render_template('blog/list.html', posts=posts)
+    entries = Entry().fetch_all()
+    return render_template('blog/list.html', entries=entries)
 
 
 @bp.route('/edit/create', methods=['GET', 'POST'])
@@ -54,12 +54,12 @@ def create():
     return render_template('blog/create.html')
 
 
-@bp.route('/edit/update/<int:post_id>', methods=['GET', 'POST'])
+@bp.route('/edit/update/<int:entry_id>', methods=['GET', 'POST'])
 @login_required
-def update(post_id):
+def update(entry_id):
     entry_client = Entry()
-    post = entry_client.fetch(post_id)
-    if post is None:
+    entry = entry_client.fetch(entry_id)
+    if entry is None:
         abort(404)
 
     if request.method == 'POST':
@@ -73,17 +73,17 @@ def update(post_id):
         if error is not None:
             flash(error)
         else:
-            entry_client.update(post_id, title, body)
+            entry_client.update(entry_id, title, body)
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', entry=entry)
 
 
-@bp.route('/edit/delete/<int:post_id>', methods=['POST'])
+@bp.route('/edit/delete/<int:entry_id>', methods=['POST'])
 @login_required
-def delete(post_id):
+def delete(entry_id):
     entry_client = Entry()
-    if entry_client.fetch(post_id) is None:
+    if entry_client.fetch(entry_id) is None:
         abort(404)
-    entry_client.delete(post_id)
+    entry_client.delete(entry_id)
     return redirect(url_for('blog.index'))
