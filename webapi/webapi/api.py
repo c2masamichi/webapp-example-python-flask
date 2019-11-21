8d829a10e71bc35005c76373dc8ada472729c505
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-11-21 23:12:17 +0900

    add model

--- webapi/webapi/api.py
+++ webapi/webapi/api.py
@@ -1,11 +1,10 @@
 import json
 
 from flask import Blueprint
-from flask import current_app
 from flask import jsonify
 from flask import request
 
-from webapi.db import get_db
+from webapi.model import Product
 
 bp = Blueprint('api', __name__)
 
@@ -17,7 +16,19 @@ def index():
 
 @bp.route('/products')
 def get_products():
-    return jsonify(load_all_data())
+    result = Product().fetch_all()
+    return jsonify(result)
+
+
+@bp.route('/products/<int:product_id>')
+def get_product(product_id):
+    result = Product().fetch(product_id)
+    if result is None:
+        error = {
+            'error': 'Not Found.'
+        }
+        return jsonify(error), 404
+    return jsonify(result)
 
 
 @bp.route('/products', methods=['POST'])
@@ -38,23 +49,8 @@ def create_product():
         }
         return jsonify(error), 400
 
-    db = get_db()
-    with db.cursor() as cursor:
-        cursor.execute(
-            'INSERT INTO product (name, price) VALUES (%s, %s)',
-            (name, price),
-        )
-    db.commit()
-    result = {
-        'result': 'Successfully Created.'
-    }
+    result = Product().create(name, price)
     return jsonify(result), 201
-
-
-@bp.route('/products/<int:product_id>')
-def get_product(product_id):
-    result, code = load_data(product_id)
-    return jsonify(result), code
 
 
 @bp.route('/products/<int:product_id>', methods=['PUT'])
@@ -75,83 +71,25 @@ def update_product(product_id):
         }
         return jsonify(error), 400
 
-    result, code = load_data(product_id)
-    if code == 404:
-        return jsonify(result), code
+    product = Product().fetch(product_id)
+    if product is None:
+        error = {
+            'error': 'Not Found.'
+        }
+        return jsonify(error), 404
 
-    db = get_db()
-    with db.cursor() as cursor:
-        cursor.execute(
-            'UPDATE product SET name = %s, price = %s WHERE id = %s',
-            (name, price, product_id),
-        )
-    db.commit()
-
-    result = {
-        'result': 'Successfully Updated.'
-    }
+    result = Product().update(product_id, name, price)
     return jsonify(result)
 
 
 @bp.route('/products/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
-    result, code = load_data(product_id)
-    if code == 404:
-        return jsonify(result), code
-
-    db = get_db()
-    with db.cursor() as cursor:
-        cursor.execute(
-            'DELETE FROM product WHERE id = %s',
-            (product_id,),
-        )
-    db.commit()
-
-    result = {
-        'result': 'Successfully Deleted.'
-    }
-    return jsonify(result)
-
-
-def load_all_data():
-    db = get_db()
-    with db.cursor() as cursor:
-        cursor.execute('SELECT * FROM product')
-        data = cursor.fetchall()
-
-    result = {
-        'result': [
-            {
-                'id': row['id'],
-                'name': row['name'],
-                'price': row['price'],
-            }
-            for row in data
-        ]
-    }
-    return result
-
-
-def load_data(product_id):
-    db = get_db()
-    with db.cursor() as cursor:
-        cursor.execute(
-            'SELECT * FROM product WHERE id = %s',
-            (product_id,),
-        )
-        row = cursor.fetchone()
-
-    if row is None:
+    product = Product().fetch(product_id)
+    if product is None:
         error = {
             'error': 'Not Found.'
         }
-        return error, 404
+        return jsonify(error), 404
 
-    result = {
-        'result': {
-            'id': row['id'],
-            'name': row['name'],
-            'price': row['price'],
-        }
-    }
-    return result, 200
+    result = Product().delete(product_id)
+    return jsonify(result)
