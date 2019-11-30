613103ee64cf486e38da124c3c8a759806862d61
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-11-30 23:55:28 +0900

    fix auth

--- cms/cms/model.py
+++ cms/cms/model.py
@@ -85,6 +85,7 @@ class User(object):
         if (user is None or 
             not check_password_hash(user['password'], password)):
             error = 'Incorrect username or password.'
+            user = None
 
         return user, error
 
