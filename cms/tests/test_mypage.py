37e87f14203a73d91d1a660141cfb0cb5d8f4d03
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-12-29 23:49:41 +0900

    password change page

---
+++ cms/tests/test_mypage.py
@@ -0,0 +1,45 @@
+import pytest
+from werkzeug.security import check_password_hash
+
+from cms.db import get_db
+
+
+@pytest.mark.parametrize(
+    'path',
+    (
+        '/mypage/chpasswd',
+    )
+)
+def test_login_required_get(client, path):
+    response = client.get(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
+
+
+def test_chpasswd(client, auth, app):
+    auth.login()
+    assert client.get('/mypage/chpasswd').status_code == 200
+
+    old_password = 'testpass'
+    new_password = 'updated'
+    response = client.post(
+        '/mypage/chpasswd',
+        data={'old_password': old_password, 'new_password': new_password}
+    )
+
+    with app.app_context():
+        db = get_db()
+        with db.cursor() as cursor:
+            cursor.execute('SELECT * FROM user WHERE id = 1')
+            user = cursor.fetchone()
+        assert check_password_hash(user['password'], new_password)
+
+
+def test_chpasswd(client, auth, app):
+    auth.login()
+    old_password = 'aaaa'
+    new_password = 'updated'
+    response = client.post(
+        '/mypage/chpasswd',
+        data={'old_password': old_password, 'new_password': new_password}
+    )
+    assert b'Incorrect password.' in response.data
