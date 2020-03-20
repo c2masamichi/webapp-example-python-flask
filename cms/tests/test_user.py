eba433bc502b87648976b470a632bff53c404e65
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2020-03-20 23:04:14 +0900

    add tests

--- cms/tests/test_user.py
+++ cms/tests/test_user.py
@@ -99,6 +99,7 @@ def test_create(client, auth, app):
         '/user/create',
         data={'role': role, 'username': username, 'password': password}
     )
+    assert response.status_code == 302
     assert 'http://localhost/user/' == response.headers['Location']
 
     with app.app_context():
@@ -138,7 +139,9 @@ def test_update(client, auth, app):
 
     auth.login()
     assert client.get(path).status_code == 200
-    client.post(path, data={'role': role, 'username': username})
+    response = client.post(path, data={'role': role, 'username': username})
+    assert response.status_code == 302
+    assert response.headers['Location'] == 'http://localhost{0}'.format(path)
 
     with app.app_context():
         db = get_db()
@@ -192,6 +195,7 @@ def test_delete(client, auth, app):
     user_id = 2
     auth.login()
     response = client.post('/user/delete/{0}'.format(user_id))
+    assert response.status_code == 302
     assert response.headers['Location'] == 'http://localhost/user/'
 
     with app.app_context():
