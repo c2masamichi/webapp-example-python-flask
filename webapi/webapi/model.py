59510d2d46255e23ef567e222dae108b9d203c9f
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2020-03-22 22:46:06 +0900

    refactoring

--- webapi/webapi/model.py
+++ webapi/webapi/model.py
@@ -1,3 +1,5 @@
+from enum import IntEnum
+
 from flask import current_app
 
 from webapi.db import get_db
@@ -8,13 +10,12 @@ class Product(object):
         self._db = get_db()
 
     def fetch_all(self):
-        result = Result()
         db = self._db
         try:
             with db.cursor() as cursor:
                 cursor.execute('SELECT * FROM product')
                 data = cursor.fetchall()
-            result.value = {
+            value = {
                 'result': [
                     {
                         'id': row['id'],
@@ -26,13 +27,14 @@ class Product(object):
             }
         except Exception as e:
             current_app.logger.error('fetching all products: {0}'.format(e))
-            result.code = 500
-            result.description = 'Fetchting all products failed.'
-        finally:
-            return result
+            return Result(
+                code=Code.INTERNAL_SERVER_ERROR,
+                description='Fetchting all products failed.'
+            )
+
+        return Result(value=value)
 
     def fetch(self, product_id):
-        result = Result()
         db = self._db
         try:
             with db.cursor() as cursor:
@@ -41,8 +43,9 @@ class Product(object):
                     (product_id,),
                 )
                 row = cursor.fetchone()
+            value = {}
             if row is not None:
-                result.value = {
+                value = {
                     'result': {
                         'id': row['id'],
                         'name': row['name'],
@@ -51,13 +54,14 @@ class Product(object):
                 }
         except Exception as e:
             current_app.logger.error('fetching product: {0}'.format(e))
-            result.code = 500
-            result.description = 'Fetchting a product failed.'
-        finally:
-            return result
+            return Result(
+                code=Code.INTERNAL_SERVER_ERROR,
+                description='Fetchting a product failed.'
+            )
+
+        return Result(value=value)
 
     def create(self, name, price):
-        result = Result()
         db = self._db
         try:
             with db.cursor() as cursor:
@@ -69,15 +73,14 @@ class Product(object):
         except Exception as e:
             db.rollback()
             current_app.logger.error('creating product: {0}'.format(e))
-            result.code = 500
-            result.description = 'Creating a product failed.'
-        else:
-            result.value = {'result': 'Successfully Created.'}
-        finally:
-            return result
+            return Result(
+                code=Code.INTERNAL_SERVER_ERROR,
+                description='Creating a product failed.'
+            )
+
+        return Result(value={'result': 'Successfully Created.'})
 
     def update(self, product_id, name, price):
-        result = Result()
         db = self._db
         try:
             with db.cursor() as cursor:
@@ -89,12 +92,12 @@ class Product(object):
         except Exception as e:
             db.rollback()
             current_app.logger.error('updating product: {0}'.format(e))
-            result.code = 500
-            result.description = 'Updating a product failed.'
-        else:
-            result.value = {'result': 'Successfully Updated.'}
-        finally:
-            return result
+            return Result(
+                code=Code.INTERNAL_SERVER_ERROR,
+                description='Updating a product failed.'
+            )
+
+        return Result(value={'result': 'Successfully Updated.'})
 
     def delete(self, product_id):
         result = Result()
@@ -109,16 +112,22 @@ class Product(object):
         except Exception as e:
             db.rollback()
             current_app.logger.error('deleting product: {0}'.format(e))
-            result.code = 500
-            result.description = 'Deleting a product failed.'
-        else:
-            result.value = {'result': 'Successfully Deleted.'}
-        finally:
-            return result
+            return Result(
+                code=Code.INTERNAL_SERVER_ERROR,
+                description='Deleting a product failed.'
+            )
+
+        return Result(value={'result': 'Successfully Updated.'})
+
+
+class Code(IntEnum):
+    OK = 200
+    BAD_REQUEST = 400
+    INTERNAL_SERVER_ERROR = 500
 
 
 class Result(object):
-    def __init__(self, code=200, description='', value=None):
+    def __init__(self, code=Code.OK, description='', value=None):
         self.code = code
         self.description = description
         if value is None:
