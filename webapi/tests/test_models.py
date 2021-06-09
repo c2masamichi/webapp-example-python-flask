b8d48b54435d50d0c17907a50492fb4aef6e973d
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-06-09 22:55:31 +0900

    rm tests case

--- webapi/tests/test_models.py
+++ webapi/tests/test_models.py
@@ -1,54 +1,6 @@
 import pytest
 
-from webapi.db import get_db
 from webapi.models import Product
-
-
-def test_fetch_all(app):
-    with app.app_context():
-        result = Product().fetch_all()
-        assert result.code == 200
-
-        products = result.value
-        assert len(products) == 2
-
-
-def test_fetch(app):
-    with app.app_context():
-        product_id = 1
-        name = 'book'
-        price = 600
-        result = Product().fetch(product_id)
-        assert result.code == 200
-
-        product = result.value
-        assert product['id'] == product_id
-        assert product['name'] == name
-        assert product['price'] == price
-
-
-def test_fetch_not_exists(app):
-    with app.app_context():
-        product_id = 10
-        result = Product().fetch(product_id)
-        assert result.value == {}
-
-
-def test_create(app):
-    with app.app_context():
-        name = 'Mineral water 500ml'
-        price = 100
-        result = Product().create(name, price)
-        assert result.code == 200
-
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute(
-                'SELECT * FROM product WHERE name = %s',
-                (name,)
-            )
-            product = cursor.fetchone()
-        assert product['price'] == price
 
 
 @pytest.mark.parametrize(
@@ -68,25 +20,6 @@ def test_create_validate(app, name, price, message):
         assert message in result.description
 
 
-def test_update(app):
-    with app.app_context():
-        product_id = 2
-        name = 'rice'
-        price = 900
-        result = Product().update(product_id, name, price)
-        assert result.code == 200
-
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute(
-                'SELECT * FROM product WHERE id = %s',
-                (product_id,)
-            )
-            product = cursor.fetchone()
-        assert product['name'] == name
-        assert product['price'] == price
-
-
 @pytest.mark.parametrize(
     ('name', 'price', 'message'),
     (
@@ -103,19 +36,3 @@ def test_update_validate(app, name, price, message):
         result = Product().update(product_id, name, price)
         assert result.code == 400
         assert message in result.description
-
-
-def test_delete(app):
-    with app.app_context():
-        product_id = 2
-        result = Product().delete(product_id)
-        assert result.code == 200
-
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute(
-                'SELECT * FROM product WHERE id = %s',
-                (product_id,)
-            )
-            product = cursor.fetchone()
-        assert product is None
