545404a68437c0d8f77f2d3f2f8ddbd77c14ad83
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-06-16 21:52:49 +0900

    use SQLAlchemy

--- cms/cms/__init__.py
+++ cms/cms/__init__.py
@@ -3,6 +3,9 @@ import sys
 
 from flask import Flask
 
+from cms.models import Entry
+from cms.models import User
+
 
 CONFIGS = {
     'production': 'config-production.py',
@@ -21,10 +24,22 @@ def create_app():
         app.logger.error('config file must exist.')
         sys.exit(1)
 
-    from cms import db, cli
+    app.config.from_mapping(
+        SQLALCHEMY_DATABASE_URI='mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(
+            app.config['DB_USER'],
+            app.config['DB_PASSWORD'],
+            app.config['DB_HOST'],
+            app.config['DB_PORT'],
+            app.config['DATABASE'],
+        ),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
 
-    db.init_app(app)
-    cli.add_cli(app)
+    from cms.database import init_app
+    from cms.cli import add_cli
+
+    init_app(app)
+    add_cli(app)
 
     from cms import auth, blog, user
 
--- cms/cms/database.py
+++ cms/cms/database.py
@@ -1,48 +1,14 @@
 from flask import current_app
-from flask import g
-import pymysql
-
-from cms.schema import SCHEMA_STATEMENTS
-from tests.data import TEST_DATA_STATEMENTS
+from flask_sqlalchemy import SQLAlchemy
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = pymysql.connect(
-            host=current_app.config['DB_HOST'],
-            port=current_app.config['DB_PORT'],
-            user=current_app.config['DB_USER'],
-            password=current_app.config['DB_PASSWORD'],
-            db=current_app.config['DATABASE'],
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor
-        )
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
-
-def init_db():
-    db = get_db()
-    with db.cursor() as cursor:
-        for statement in SCHEMA_STATEMENTS:
-            cursor.execute(statement)
-    db.commit()
-
-
-def load_data():
-    db = get_db()
-    with db.cursor() as cursor:
-        for statement in TEST_DATA_STATEMENTS:
-            cursor.execute(statement)
-    db.commit()
+db = SQLAlchemy()
 
 
 def init_app(app):
-    app.teardown_appcontext(close_db)
+    db.init_app(app)
+
+
+def init_db():
+    db.drop_all(app=current_app)
+    db.create_all(app=current_app)
