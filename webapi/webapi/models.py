55bed6520f2f2beb12f25ed84b788013a6f3f71e
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-06-28 23:51:49 +0900

    fix var

--- webapi/webapi/models.py
+++ webapi/webapi/models.py
@@ -11,18 +11,18 @@ class Product(db.Model):
     price = db.Column(db.Integer, nullable=False)
 
     @validates('name')
-    def validate_name(self, key, product):
+    def validate_name(self, key, name):
         min_length = 3
         max_length = 20
         pattern = r'[0-9a-zA-Z ]*'
 
-        assert min_length <= len(product) <= max_length
-        assert re.fullmatch(pattern, product) is not None
-        return product
+        assert min_length <= len(name) <= max_length
+        assert re.fullmatch(pattern, name) is not None
+        return name
 
     @validates('price')
-    def validate_price(self, key, product):
+    def validate_price(self, key, price):
         min_num = 0
         max_num = 1000000000
-        assert min_num <= product <= max_num
-        return product
+        assert min_num <= price <= max_num
+        return price
