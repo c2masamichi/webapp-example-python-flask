1418889257a554588de7b467e1cf2a1e999d73de
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-07-06 22:34:04 +0900

    fix tests

--- cms/tests/test_models_entry.py
+++ cms/tests/test_models_entry.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 
-from cms.db import get_db
+import pytest
+
+from cms.database import db
 from cms.models import Entry
 
 
 def test_fetch_all(app):
     with app.app_context():
-        result = Entry().fetch_all()
-        assert result.succeeded
-
-        entries = result.value
+        entries = Entry.query.all()
         assert len(entries) == 3
 
         # sorted by created desc
@@ -21,24 +20,11 @@ def test_fetch_all(app):
 def test_fetch(app):
     with app.app_context():
         entry_id = 1
-        result = Entry().fetch(entry_id)
-        assert result.succeeded
-
-        entry = result.value
-        assert entry['id'] == entry_id
-        assert entry['title'] == 'Test Title 1'
-        assert entry['body'] == 'This body is test.'
-        assert entry['created'] == datetime(2019, 1, 1, 0, 0)
-
-
-def test_fetch_not_exists(app):
-    with app.app_context():
-        entry_id = 5
-        result = Entry().fetch(entry_id)
-        assert result.succeeded
-
-        entry = result.value
-        assert entry is None
+        entry = Entry.query.get(entry_id)
+        assert entry.id == entry_id
+        assert entry.title == 'Test Title 1'
+        assert entry.body == 'This body is test.'
+        assert entry.created == datetime(2019, 1, 1, 0, 0)
 
 
 def test_create(app):
@@ -46,27 +32,19 @@ def test_create(app):
         author_id = 1
         title = 'created'
         body = 'created on test'
-        result = Entry().create(author_id, title, body)
-        assert result.succeeded
-
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute('SELECT * FROM entry WHERE id = 4')
-            entry = cursor.fetchone()
-        assert entry['author_id'] == author_id
-        assert entry['title'] == title
-        assert entry['body'] == body
+        entry = Entry(title=title, body=body, author_id=author_id)
+        db.session.add(entry)
+        db.session.commit()
+        assert entry.id == 4
 
 
 def test_create_validate(app):
     with app.app_context():
-        author_id = 1
-        title = 'a' * 101
-        body = 'created on test'
-        message = 'Bad data'
-        result = Entry().create(author_id, title, body)
-        assert not result.succeeded
-        assert message in result.description
+        with pytest.raises(AssertionError):
+            author_id = 1
+            title = 'a' * 101
+            body = 'created on test'
+            Entry(title=title, body=body, author_id=author_id)
 
 
 def test_update(app):
@@ -74,42 +52,26 @@ def test_update(app):
         entry_id = 1
         title = 'updated'
         body = 'updated on test'
-        result = Entry().update(entry_id, title, body)
-        assert result.succeeded
-
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute(
-                'SELECT * FROM entry WHERE id = %s',
-                (entry_id,)
-            )
-            entry = cursor.fetchone()
-        assert entry['title'] == title
-        assert entry['body'] == body
+        entry = Entry.query.get(entry_id)
+        entry.title = title
+        entry.body = body
+        db.session.commit()
 
 
 def test_update_validate(app):
     with app.app_context():
-        entry_id = 1
-        title = 'a' * 101
-        body = 'updated on test'
-        message = 'Bad data'
-        result = Entry().update(entry_id, title, body)
-        assert not result.succeeded
-        assert message in result.description
+        with pytest.raises(AssertionError):
+            entry_id = 1
+            title = 'a' * 101
+            body = 'updated on test'
+            entry = Entry.query.get(entry_id)
+            entry.title = title
+            entry.body = body
 
 
 def test_delete(app):
     with app.app_context():
         entry_id = 1
-        result = Entry().delete(entry_id)
-        assert result.succeeded
-
-        db = get_db()
-        with db.cursor() as cursor:
-            cursor.execute(
-                'SELECT * FROM entry WHERE id = %s',
-                (entry_id,)
-            )
-            entry = cursor.fetchone()
-        assert entry is None
+        entry = Entry.query.get(entry_id)
+        db.session.delete(entry)
+        db.session.commit()
