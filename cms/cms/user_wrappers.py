0a205836cde14582b8f2038d125901a1608916af
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-07-17 23:58:40 +0900

    add user_wrappers

---
+++ cms/cms/user_wrappers.py
@@ -0,0 +1,18 @@
+from werkzeug.security import check_password_hash
+
+from cms.models import User
+
+
+def auth_user(name, password):
+    """Auth user.
+    Args:
+        name (str): user's name
+        password (str): user's password
+    Returns:
+        user: User instance
+    """
+
+    user = User.query.filter_by(name=name).first()
+    if check_password_hash(user['password'], password):
+        return None
+    return user
