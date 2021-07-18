1410820c9d8d9414b04bae6936b185560a01b3e2
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-07-18 23:32:51 +0900

    change_password

--- cms/cms/user_wrappers.py
+++ cms/cms/user_wrappers.py
@@ -1,5 +1,7 @@
 from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
+from cms.database import db
 from cms.models import User
 
 
@@ -17,3 +19,32 @@ def auth_user(name, password):
             not check_password_hash(user.password, password)):
         return None
     return user
+
+
+def change_password(
+        user_id, new_password, old_password=None, old_required=True):
+    """Change password.
+    Args:
+        user_id (int): id of user to change
+        new_password (str): password after change
+        old_password (str): current password
+        old_required (bool): True if current password must be checked
+    Returns:
+        succeeded (bool): True if password successfully changed
+        message (str): description of result
+    """
+    default_err_msg = 'Incorrect password.'
+    if old_required and old_password is None:
+        return False, default_err_msg
+
+    user = User.query.get(user_id)
+    if user is None:
+        return False, 'Update failed.'
+
+    if (old_required and
+            not check_password_hash(user.password, old_password)):
+        return False, default_err_msg
+
+    user.password = generate_password_hash(new_password)
+    db.session.commit()
+    return True, 'Password Changed.'
--- cms/tests/test_user_wrappers.py
+++ cms/tests/test_user_wrappers.py
@@ -1,6 +1,7 @@
 import pytest
 
 from cms.user_wrappers import auth_user
+from cms.user_wrappers import change_password
 
 
 def test_auth(app):
@@ -23,3 +24,17 @@ def test_auth_user_validate(app, name, password):
     with app.app_context():
         user = auth_user(name, password)
         assert user is None
+
+
+def test_change_password(app):
+    with app.app_context():
+        user_id = 2
+        name = 'user-editor01'
+        new_password = 'updated-pass_01'
+        succeeded, message = change_password(
+            user_id, new_password, old_required=False)
+        assert succeeded
+        assert 'Password Changed.' in message
+
+        user = auth_user(name, new_password)
+        assert user is not None
