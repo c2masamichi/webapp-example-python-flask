b40aafda19d50e373c4f4569f35036970b405cb4
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2021-08-11 23:47:13 +0900

    retry db

--- webapi/webapi/database.py
+++ webapi/webapi/database.py
@@ -1,6 +1,8 @@
+import time
+
 from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.exc import OperationalError
 
 db = SQLAlchemy()
 
@@ -9,6 +11,18 @@ def init_app(app):
     db.init_app(app)
 
 
-def init_db():
-    db.drop_all(app=current_app)
-    db.create_all(app=current_app)
+def init_db(num_retries=2, retry_interval=5):
+    for retry in range(num_retries + 1):
+        try:
+            db.drop_all(app=current_app)
+            db.create_all(app=current_app)
+        except OperationalError as err:
+            current_app.logger.error(
+                'sqlalchemy.exc.OperationalError: {0}'.format(err))
+            current_app.logger.error(
+                'retrying operation: {0}'.format(retry + 1))
+            if retry >= num_retries:
+                raise Exception('over num_retries') from None
+            time.sleep(retry_interval)
+        else:
+            break
