35eff260a7239338d2822d348f63de5468f98db9
c2masamichi <19793735+c2masamichi@users.noreply.github.com>
2022-08-13 22:31:43 +0900

    fix error handlers

--- cms/app/cms/__init__.py
+++ cms/app/cms/__init__.py
@@ -41,7 +41,6 @@ def create_app():
     app.register_error_handler(400, eh.bad_request)
     app.register_error_handler(403, eh.forbidden)
     app.register_error_handler(404, eh.not_found)
-    app.register_error_handler(500, eh.internal_server_error)
 
     from cms.database import init_app
     from cms.cli import add_cli
--- cms/app/cms/error_handler.py
+++ cms/app/cms/error_handler.py
@@ -2,16 +2,12 @@ from flask import render_template
 
 
 def bad_request(e):
-    return render_template('error/400.html'), 404
+    return render_template('error/400.html'), 400
 
 
 def forbidden(e):
-    return render_template('error/403.html'), 404
+    return render_template('error/403.html'), 403
 
 
 def not_found(e):
     return render_template('error/404.html'), 404
-
-
-def internal_server_error(e):
-    return render_template('error/500.html'), 404
